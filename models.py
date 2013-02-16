b63b27bff3fc99e10895b01150e83fea8a60782c
guth <guthriea90@gmail.com>
2013-02-16 18:48:30 -0500

    Cleaned up urls.

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Problem(models.Model):
-	name = models.CharField(max_length=10)
+	name = models.CharField(max_length=10, primary_key=True)
 	description = models.TextField()
 	problemStatement = models.TextField()
 	publishedDate = models.DateTimeField()
 
 	def __unicode__(self):
 		return self.name
+
+	def get_absolute_url(self):
+		return '/prog/problems/%s' % self.name
