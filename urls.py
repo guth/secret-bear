bbee505fea6846fc56d97b83a8709cbb8c77a3b6
guth <guthriea90@gmail.com>
2013-02-18 19:29:51 -0500

    Decoupled urls by not hardcoding them into templates. Switched to using {% url %} tag to create the url.

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
- 	url(r'^$', 'programmer.views.main'),
-	url(r'^problems$', 'programmer.views.allProblems'),
-	url(r'^problems/([A-Z]+)$', 'programmer.views.problemDetail'),
-	url(r'^problems/judge/([A-Z]+)$', 'programmer.views.judge'),
+urlpatterns = patterns('programmer.views',
+ 	url(r'^$', 'main', name='mainView'),
+	url(r'^problems$', 'allProblems', name='allProblemsView'),
+	url(r'^problems/([A-Z]+)$', 'problemDetail', name='problemDetailView'),
+	url(r'^problems/judge/([A-Z]+)$', 'judge', name='judgeView'),
 )
