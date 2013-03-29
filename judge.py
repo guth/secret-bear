75b06762cbf9f0c65fd56d0e28bd7f41529845b4
guth <guthriea90@gmail.com>
2013-03-29 18:49:21 -0400

    Removed dependency on the Envoy package and added the code to judge.py for future re-writing & extension.

--- judge.py
+++ judge.py
@@ -1,6 +1,11 @@
-import envoy
 import logging
 import os
+import shlex
+import signal
+import subprocess
+import sys
+import threading
+
 import status
 
 from datetime import datetime
@@ -15,13 +20,153 @@ langToExt[PYTHON] = PYTHON_EXT
 
 log = logging.getLogger(__name__)
 
+def terminate_process(process):
+	os.kill(process.pid, signal.SIGTERM)
+
+def kill_process(process):
+	os.kill(process.pid, signal.SIGKILL)
+
+def thread_is_alive(thread):
+	if hasattr(thread, "is_alive"):
+		return thread.is_alive()
+	else:
+		return thread.isAlive()
+
+class Command(object):
+	def __init__(self, cmd):
+		self.cmd = cmd
+		self.process = None
+		self.out = None
+		self.err = None
+		self.returncode = None
+		self.data = None
+		self.exc = None
+
+	def run(self, data, timeout, kill_timeout, env, cwd):
+		self.data = data
+		environ = dict(os.environ)
+		environ.update(env or {})
+
+		def target():
+			try:
+				self.process = subprocess.Popen(self.cmd,
+					universal_newlines=True,
+					shell=False,
+					env=environ,
+					stdin=subprocess.PIPE,
+					stdout=subprocess.PIPE,
+					stderr=subprocess.PIPE,
+					bufsize=0,
+					cwd=cwd,
+				)
+
+				if sys.version_info[0] >= 3: # Python 3 support
+					self.out, self.err = self.process.communicate(
+						input = bytes(self.data, "UTF-8") if self.data else None 
+					)
+				else:
+					self.out, self.err = self.process.communicate(self.data)
+			except Exception as exc:
+				self.exc = exc
+		
+		thread = threading.Thread(target=target)
+
+		thread.start()
+		thread.join(timeout)
+
+		if self.exc:
+			raise self.exc
+
+		if thread_is_alive(thread):
+			terminate_process(self.process)
+			thread.join(kill_timeout)
+			if thread_is_alive(thread):
+				kill_process(self.process)
+				thread.join()
+
+		self.returncode = self.process.returncode
+
+		return self.out, self.err
+
+class Response(object):
+	"""An executed command's response"""
+
+	def __init__(self, process=None):
+		super(Response, self).__init__()
+
+		self.process = process
+		self.command = None
+		self.std_err = None
+		self.std_out = None
+		self.status_code = None
+		self.history = []
+
+
+	def __repr__(self):
+		if len(self.command):
+			return '<Response [{0}]>'.format(self.command[0])
+		else:
+			return '<Response>'
+
+def expand_args(command):
+    """Parses command strings and returns a Popen-ready list."""
+
+    # Prepare arguments.
+    if isinstance(command, str):
+        splitter = shlex.shlex(command)
+        splitter.whitespace = '|'
+        splitter.whitespace_split = True
+        command = []
+
+        while True:
+            token = splitter.get_token()
+            if token:
+                command.append(token)
+            else:
+                break
+
+        command = list(map(shlex.split, command))
+
+    return command
+
+def run_command(command, data=None, timeout=None, kill_timeout=None, env=None, cwd=None):
+    """Executes a given commmand and returns Response.
+
+    Blocks until process is complete, or timeout is reached.
+    """
+
+    command = expand_args(command)
+    history = []
+    for c in command:
+
+        if len(history):
+            # due to broken pipe problems pass only first 10 KiB
+            data = history[-1].std_out[0:10*1024]
+
+        cmd = Command(c)
+        out, err = cmd.run(data, timeout, kill_timeout, env, cwd)
+
+        r = Response(process=cmd)
+
+        r.command = c
+        r.std_out = out
+        r.std_err = err
+        r.status_code = cmd.returncode
+
+        history.append(r)
+
+    r = history.pop()
+    r.history = history
+
+    return r
+
 def runPython(fileName, stdin, expectedOutput):
 	""" Runs the given Python file with the given input. Returns
 	the proper status code based on the run succeeding and the
 	expected output being matched. """
 
 	execArg =  "python %s" % fileName
-	r = envoy.run(execArg, data=stdin, timeout=DEFAULT_TIMEOUT)
+	r = run_command(execArg, data=stdin, timeout=DEFAULT_TIMEOUT)
 	return getResult(r, expectedOutput)
 	
 def runJava(fileName, stdin, expectedOutput):
@@ -30,7 +175,7 @@ def runJava(fileName, stdin, expectedOutput):
 	and the expected output being matched. """
 
 	compileArg = "javac %s" % fileName
-	r = envoy.run(compileArg, timeout=DEFAULT_TIMEOUT)
+	r = run_command(compileArg, timeout=DEFAULT_TIMEOUT)
 	
 	if r.statusCode != 0:
 		return status.COMPILE_ERROR
@@ -39,7 +184,7 @@ def runJava(fileName, stdin, expectedOutput):
 	progName = fileName[:i]
 	execArg = "java %s" % progName
 
-	r = envoy.run(execArg, data=stdin, timeout=DEFAULT_TIMEOUT)
+	r = run_command(execArg, data=stdin, timeout=DEFAULT_TIMEOUT)
 	return getResult(r, expectedOutput)
 
 extToMethod = {}
