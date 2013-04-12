077c0319d6e0180eaede346c03ad5aecdae0badc
guth <guthriea90@gmail.com>
2013-04-12 11:10:21 -0400

    Added tests for running the judge in a new process.

--- unittests/JudgeTest.py
+++ unittests/JudgeTest.py
@@ -10,6 +10,18 @@ class JudgeTest(TestCase):
 	def setUp(self):
 		self.HELLO_WORLD = "Hello, world!"
 
+	def test_get_python_commands(self):
+		cmds = judge.getPythonCommands("main.py")
+
+		self.assertEqual(cmds.compileCmd, None)
+		self.assertEqual(cmds.runCmd, "python main.py")
+
+	def test_get_java_commands(self):
+		cmds = judge.getJavaCommands("Main.java")
+
+		self.assertEqual(cmds.compileCmd, "javac Main.java")
+		self.assertEqual(cmds.runCmd, "java Main")
+
 	def test_get_ac_result(self):
 		dr = DummyResult(0, self.HELLO_WORLD)
 		result = judge.getResult(dr, self.HELLO_WORLD)
@@ -40,5 +52,23 @@ class JudgeTest(TestCase):
 		stdin = ""
 		expectedOutput = self.HELLO_WORLD
 		result = judge.executeProgram(source, language, stdin, expectedOutput)
+
+		self.assertEqual(result, status.ANSWER_CORRECT)
+
+	def test_execute_in_new_process(self):
+		source = "print '%s'" % self.HELLO_WORLD
+		language = languages.PYTHON
+		stdin = ""
+		expectedOutput = self.HELLO_WORLD
+		result = judge.executeProgram(source, language, stdin, expectedOutput)
+
+		self.assertEqual(result, status.ANSWER_CORRECT)
+
+	def test_subprocess_judge(self):
+		source = "print '%s'" % self.HELLO_WORLD
+		language = languages.PYTHON
+		stdin = ""
+		expectedOutput = self.HELLO_WORLD
+		result = judge.subprocessJudge(source, language, stdin, expectedOutput)
 
 		self.assertEqual(result, status.ANSWER_CORRECT)
